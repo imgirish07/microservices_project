6cbd85157ab1e63a64f5f8efdd1d7e023f850324
imgirish07 <girishkumargkgk97@gmail.com>
2025-01-18 18:23:20 +0530

    added ingress-nginx, skaffold files

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import PostList from "./PostList";
 const App = () => {
   return (
     <div className="container">
-      <h1>Create Post</h1>
+      <h1>Create Post and add comments!</h1>
       <PostCreate />
       <hr />
       <h1>Posts</h1>
